400ac4930163dd619558e56f7ca0de7b87e83f1d
Mykhailo Yurkiv <mishayurkiv15@gmail.com>
2023-07-31 21:34:59 +0300

    fixed congratulation

--- js/form.js
+++ js/form.js
@@ -9,14 +9,26 @@ form.addEventListener("submit", () => {
   localStorage.setItem("Surname", surname.value);
   localStorage.setItem("E-mail", email.value);
   if (localStorage.getItem("Name") === "Sigma") {
-    congratulation();
+    congratulation(localStorage.getItem("Name"));
   }
 });
 button.addEventListener("click", () => {});
-function congratulation() {
+function congratulation(name) {
   let congratulationsWrapper = document.querySelector(
     ".congratulations__wrapper"
   );
+  let curentDate = new Date();
+  let congratulationDate = document.querySelector(".congratulations__date");
+  let congratulationName = document.querySelector(".congratulations__name");
+  function addZero(num) {
+    if (num < 10) return "0" + num;
+    else return num;
+  }
+  let day = addZero(curentDate.getDate());
+  let month = addZero(curentDate.getMonth()+1);
+  let year = curentDate.getFullYear();
+  congratulationDate.innerHTML = `${day}/${month}/${year}`;
+  congratulationName.innerHTML = name
   congratulationsWrapper.style.display = "block";
   setTimeout(() => {
     congratulationsWrapper.style.opacity = "1";
