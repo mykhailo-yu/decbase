30b39f2bdacac7d0983b997b883635ed3a224947
Mykhailo Yurkiv <mishayurkiv15@gmail.com>
2023-08-01 16:39:45 +0300

    added form validation

--- js/form.js
+++ js/form.js
@@ -11,8 +11,45 @@ form.addEventListener("submit", () => {
   if (localStorage.getItem("Name") === "Sigma") {
     congratulation(localStorage.getItem("Name"));
   }
+  username.value = "";
+  surname.value = "";
+  email.value = "";
+  [surname, username, email].forEach(()=>{removeEventListener("input",()=>{})});
 });
-button.addEventListener("click", () => {});
+button.addEventListener("click", ()=>{
+  checkValidity();
+  [surname, username, email].forEach((el) => {
+    el.addEventListener("input", () => {
+      checkValidity();
+    });
+  });
+});
+function checkValidity() {
+  if (username.validity.patternMismatch) {
+    username.setCustomValidity(
+      "Введіть ваше ім'я з великої літери, всі інші літери малі, \n без пробілів, використовуючи тільки латинські літери!"
+    );
+    username.classList.add("sign-up-form__input--invalid");
+  } else {
+    username.setCustomValidity("");
+    username.classList.remove("sign-up-form__input--invalid");
+  }
+  if (surname.validity.patternMismatch) {
+    surname.setCustomValidity(
+      "Введіть ваше прізвище з великої літери, без пробілів, \n використовуючи тільки латинські літери!"
+    );
+    surname.classList.add("sign-up-form__input--invalid");
+  } else {
+    surname.setCustomValidity("");
+    surname.classList.remove("sign-up-form__input--invalid");
+  }
+  if (email.validity.typeMismatch) {
+    email.classList.add("sign-up-form__input--invalid");
+  } else {
+    email.setCustomValidity("");
+    email.classList.remove("sign-up-form__input--invalid");
+  }
+}
 function congratulation(name) {
   let congratulationsWrapper = document.querySelector(
     ".congratulations__wrapper"
@@ -25,10 +62,10 @@ function congratulation(name) {
     else return num;
   }
   let day = addZero(curentDate.getDate());
-  let month = addZero(curentDate.getMonth()+1);
+  let month = addZero(curentDate.getMonth() + 1);
   let year = curentDate.getFullYear();
   congratulationDate.innerHTML = `${day}/${month}/${year}`;
-  congratulationName.innerHTML = name
+  congratulationName.innerHTML = name;
   congratulationsWrapper.style.display = "block";
   setTimeout(() => {
     congratulationsWrapper.style.opacity = "1";
